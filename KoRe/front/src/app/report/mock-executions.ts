1812d0055bac554edfbce1c3f14abc4eb90b784f
Malek-MBE <62557333+Malek-MBE@users.noreply.github.com>
2020-03-31 08:35:05 +0200

    Add files via upload

---
+++ KoRe/front/src/app/report/mock-executions.ts
@@ -0,0 +1,28 @@
+
+import { Executions } from '../custom-classes';
+
+
+
+export const EXECUTIONS: Executions[] = [
+    {
+      order: 3,
+      name: 'log3',
+      value: 'log_25032020_100312.log',
+      time : '35 minutes',
+      path : 'D:/CACIB/2020_FRAIS.pdf'
+    },
+    {
+      order: 2,
+      name: 'log2',
+      value: 'log_25032020_100315.log',
+      time : '30 minutes and 45 secondes',
+      path : 'D:/CACIB/2020_FRAIS.pdf'
+    },
+    {
+      order: 1,
+      name: 'log1',
+      value: 'log_29032020_100315.log',
+      time : '30 minutes and 45 secondes',
+      path : 'D:/CACIB/2020_FRAIS.pdf'
+    }
+  ];
---
+++ KoRe/front/src/app/report/mock-postprocess.ts
@@ -0,0 +1,19 @@
+import { PostProcess } from '../custom-classes';
+
+export const POSTPROCESSES: PostProcess[] = [
+    {
+      order: 1,
+      name: 'Rename output file',
+      value: 'Rename output file'
+    },
+    {
+      order: 2,
+      name: 'Action2',
+      value: 'Action2'
+    },
+    {
+      order: 3,
+      name: 'Action3',
+      value: 'Action3'
+    }
+  ];
