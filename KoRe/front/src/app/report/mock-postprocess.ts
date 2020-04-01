ee604296a3f68519bd8eb7b6ab2717525ea2cb8c
Malek-MBE <62557333+Malek-MBE@users.noreply.github.com>
2020-04-02 00:12:51 +0200

    Update mock-postprocess.ts

--- KoRe/front/src/app/report/mock-postprocess.ts
+++ KoRe/front/src/app/report/mock-postprocess.ts
@@ -3,17 +3,8 @@ import { PostProcess } from '../custom-classes';
 export const POSTPROCESSES: PostProcess[] = [
     {
       order: 1,
-      name: 'Rename output file',
-      value: 'Rename output file'
-    },
-    {
-      order: 2,
-      name: 'Action2',
-      value: 'Action2'
-    },
-    {
-      order: 3,
-      name: 'Action3',
-      value: 'Action3'
+      name: 'Execute custom script',
+      value: 'cp $file $HOME/GENERATION_FILES/EXPECTPAY/.'
     }
+    
   ];
