81ab1954c525135d444c76cc7fc53720f9b680fb
Malek-MBE <62557333+Malek-MBE@users.noreply.github.com>
2020-04-02 00:11:30 +0200

    Update view-postprocess.component.ts

--- KoRe/front/src/app/report/view-postprocess/view-postprocess.component.ts
+++ KoRe/front/src/app/report/view-postprocess/view-postprocess.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
 import { PostProcess } from '../../custom-classes';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-view-postprocess',
@@ -10,9 +11,40 @@ export class ViewPostprocessComponent implements OnInit {
 
   @Input() postProcesses: PostProcess[];
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  openDialog(content: string): void {
+    this.dialog.open(OutputPostProcessDialog, {
+      minWidth: '90%',
+      data: content
+    });
+
+  }
+}
+
+@Component({
+  selector: 'dialog-overview-post-dialog',
+  template: `
+    <h1 mat-dialog-title>Script's content</h1>
+    <div mat-dialog-content>
+      <p> {{data}} </p>
+    </div>
+    <div mat-dialog-actions>
+      <button color="warn" mat-stroked-button (click)="onCloseClick()">Close</button>
+    </div>
+  `,
+})
+export class OutputPostProcessDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<OutputPostProcessDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: string) { }
+
+  onCloseClick(): void {
+    this.dialogRef.close();
   }
 
 }
