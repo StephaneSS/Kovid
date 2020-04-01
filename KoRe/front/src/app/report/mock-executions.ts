b680f7fa274d1841a2b25d5a04a8478b82acaadb
Malek-MBE <62557333+Malek-MBE@users.noreply.github.com>
2020-04-02 00:12:18 +0200

    Update mock-executions.ts

--- KoRe/front/src/app/report/mock-executions.ts
+++ KoRe/front/src/app/report/mock-executions.ts
@@ -5,24 +5,111 @@ import { Executions } from '../custom-classes';
 
 export const EXECUTIONS: Executions[] = [
     {
+      order: 13,
+      name: 'log_25032020_131252.log',
+      startDate : "25/03/2020 13:12:52",
+      endDate : "25/03/2020 13:17:52",
+      duration : '4 minutes',
+      status : "OK",
+      path : 'path'
+    },
+    {
+      order: 12,
+      name: 'log_10032020_100312.log',
+      startDate : "10/03/2020 10:31:12",
+      endDate : "10/03/2020 10:35:52",
+      duration : '4 minutes',
+      status : "KO",
+      path : 'path'
+    },
+    {
+      order: 11,
+      name: 'log_01032020_111252.log',
+      startDate : "01/03/2020 11:12:52",
+      endDate : "01/03/2020 11:14:52",
+      duration : '2 minutes',
+      status : "OK",
+      path : 'path'
+    },
+    {
+      order: 10,
+      name: 'log_25032020_131252.log',
+      startDate : "25/03/2020 13:12:52",
+      endDate : "25/03/2020 13:17:52",
+      duration : '4 minutes',
+      status : "OK",
+      path : 'path'
+    },
+    {
+      order: 9,
+      name: 'log_10032020_100312.log',
+      startDate : "10/03/2020 10:31:12",
+      endDate : "10/03/2020 10:35:52",
+      duration : '4 minutes',
+      status : "KO",
+      path : 'path'
+    },
+    {
+      order: 8,
+      name: 'log_01032020_111252.log',
+      startDate : "01/03/2020 11:12:52",
+      endDate : "01/03/2020 11:14:52",
+      duration : '2 minutes',
+      status : "OK",
+      path : 'path'
+    },
+    {
+      order: 7,
+      name: 'log_25032020_131252.log',
+      startDate : "25/03/2020 13:12:52",
+      endDate : "25/03/2020 13:17:52",
+      duration : '4 minutes',
+      status : "OK",
+      path : 'path'
+    },
+    {
+      order: 6,
+      name: 'log_10032020_100312.log',
+      startDate : "10/03/2020 10:31:12",
+      endDate : "10/03/2020 10:35:52",
+      duration : '4 minutes',
+      status : "KO",
+      path : 'path'
+    },
+    {
+      order: 5,
+      name: 'log_01032020_111252.log',
+      startDate : "01/03/2020 11:12:52",
+      endDate : "01/03/2020 11:14:52",
+      duration : '2 minutes',
+      status : "OK",
+      path : 'path'
+    },
+    {
       order: 3,
-      name: 'log3',
-      value: 'log_25032020_100312.log',
-      time : '35 minutes',
+      name: 'log_25032020_131252.log',
+      startDate : "25/03/2020 13:12:52",
+      endDate : "25/03/2020 13:17:52",
+      duration : '4 minutes',
+      status : "OK",
       path : 'path'
     },
     {
       order: 2,
-      name: 'log2',
-      value: 'log_25032020_100315.log',
-      time : '30 minutes and 45 secondes',
+      name: 'log_10032020_100312.log',
+      startDate : "10/03/2020 10:31:12",
+      endDate : "10/03/2020 10:35:52",
+      duration : '4 minutes',
+      status : "KO",
       path : 'path'
     },
     {
       order: 1,
-      name: 'log1',
-      value: 'log_29032020_100315.log',
-      time : '30 minutes and 45 secondes',
+      name: 'log_01032020_111252.log',
+      startDate : "01/03/2020 11:12:52",
+      endDate : "01/03/2020 11:14:52",
+      duration : '2 minutes',
+      status : "OK",
       path : 'path'
     }
   ];
