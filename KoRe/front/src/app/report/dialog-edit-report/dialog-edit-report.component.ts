1cbdbbbc3c1aef8a26e014ad6b9125b179832cc4
cregis <regis.cassier@u-psud.fr>
2020-04-02 10:12:07 +0200

    return result to global edit dialog

--- KoRe/front/src/app/report/dialog-edit-report/dialog-edit-report.component.ts
+++ KoRe/front/src/app/report/dialog-edit-report/dialog-edit-report.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import * as cloneDeep from 'lodash/cloneDeep';
 import { Report } from '../../custom-classes';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-edit-report',
@@ -13,6 +14,8 @@ export class DialogEditReportComponent implements OnInit {
   src_report: Report;
   edited_report: Report;
 
+  reportForm: FormGroup;
+
   constructor(
     public dialogRef: MatDialogRef<DialogEditReportComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Report) {}
@@ -20,6 +23,7 @@ export class DialogEditReportComponent implements OnInit {
   ngOnInit(): void {
     this.src_report = this.data; 
     this.edited_report = cloneDeep(this.src_report);
+    this.reportForm = new FormGroup({});
   }
 
 }
--- KoRe/front/src/app/report/edit-report/edit-report.component.ts
+++ KoRe/front/src/app/report/edit-report/edit-report.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Report } from '../../custom-classes';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-report',
@@ -14,16 +14,36 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class EditReportComponent implements OnInit {
 
   @Input() report: Report;
+  @Output() reportChanged: EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
 
   constructor(private readonly formBuilder: FormBuilder) { }
 
   reportForms: FormGroup = new FormGroup({
     schedules: this.formBuilder.array([]),
     inputArgs: this.formBuilder.array([]),
+    postProcesses: this.formBuilder.array([]),
     destinations: this.formBuilder.array([])
   });
 
   ngOnInit(): void {
+    this.reportForms.addControl('name', new FormControl(this.report.name, [Validators.required]) );
+    this.reportForms.addControl('description', new FormControl(this.report.description, []));
+    this.reportForms.addControl('output', new FormControl(this.report.output, []));
+    this.reportForms.markAllAsTouched();
+  }
+
+  valueChanged(selector: string, value: FormGroup|FormControl): void{
+    this.reportForms.removeControl(selector);
+    this.reportForms.addControl(selector,value);
+    this.notifyChanges();
+  }
+
+  valueChangedWithin(){
+    this.notifyChanges();
+  }
+
+  notifyChanges(): void {
+    this.reportChanged.emit(this.reportForms);
   }
 
 }
