007e704d71d3266f615a2ae2e9e149d0342338ae
cregis <regis.cassier@u-psud.fr>
2020-05-19 09:28:56 +0200

    reduce report controller dependencies by binding it to a service

--- KoRe/back/src/main/java/com/tbd/kore/controller/ReportController.java
+++ KoRe/back/src/main/java/com/tbd/kore/controller/ReportController.java
@@ -2,95 +2,53 @@ package com.tbd.kore.controller;
 
 import com.tbd.kore.model.report.Report;
 import com.tbd.kore.model.report.ReportSimple;
-import com.tbd.kore.model.report.Schedule;
-import com.tbd.kore.repository.ReportRepository;
-import com.tbd.kore.service.JobRunnerService;
-import com.tbd.kore.service.ScheduleTaskService;
+import com.tbd.kore.service.ReportServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
-
 @RestController
 @RequestMapping("/report")
 public class ReportController {
 
     @Autowired
-    private ReportRepository reportRepository;
-
-    @Autowired
-    private JobRunnerService jobRunnerService;
-
-    @Autowired
-    private ScheduleTaskService scheduleTaskService;
+    private ReportServiceImpl reportService;
 
     @GetMapping(value="/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ReportSimple>> getAllReports() {
-        List<ReportSimple> reports = reportRepository.findAll()
-                .stream()
-                .map(ReportSimple::new)
-                .collect(Collectors.toList());
-        return new ResponseEntity<>(reports, HttpStatus.OK);
+        return new ResponseEntity<>(reportService.getAllSimple(), HttpStatus.OK);
     }
 
     @PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Report> addReport(@RequestBody Report report) {
-        report.setId(null);
-        report.setExecutionLogs(new ArrayList<>());
-        Report savedReport = reportRepository.save(report);
-        savedReport.getSchedules().forEach(jobRunnerService::addRunnerFromSchedule);
-        return new ResponseEntity<>(savedReport, HttpStatus.CREATED);
+        return new ResponseEntity<>(reportService.add(report), HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Report> getReportById(@PathVariable("id") Long id) {
-        return reportRepository.findById(id)
-                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+        return reportService.getById(id)
+                .map( report -> new ResponseEntity<>(report, HttpStatus.OK) )
+                .orElse( ResponseEntity.status(HttpStatus.NOT_FOUND).build() );
     }
 
     @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Report> updateReportById(@PathVariable("id") Long id, @RequestBody Report report) {
-        Optional<Report> optReport = reportRepository.findById(id);
-        if (optReport.isPresent()) {
-            Report oldReport = optReport.get();
-            if(report.getName() != null && !report.getName().trim().isEmpty() ) {
-                oldReport.setName(report.getName());
-            }
-            if(report.getDescription() != null && !report.getDescription().trim().isEmpty()) {
-                oldReport.setDescription(report.getDescription());
-            }
-            oldReport.setArguments(report.getArguments());
-            oldReport.getSchedules().stream().map(Schedule::getId).forEach(scheduleTaskService::removeTaskFromScheduler);
-            oldReport.setSchedules(report.getSchedules());
-            Report savedReport = reportRepository.save(oldReport);
-            savedReport.getSchedules().forEach(jobRunnerService::addRunnerFromSchedule);
-            return new ResponseEntity<>(savedReport, HttpStatus.OK);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+        return reportService.updateById(id, report)
+                .map( updatedReport -> new ResponseEntity<>(updatedReport, HttpStatus.OK) )
+                .orElse( ResponseEntity.status(HttpStatus.NOT_FOUND).build() );
     }
 
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> deleteReportById(@PathVariable("id") Long id) {
 
-        AtomicReference<HttpStatus> responseStatus = new AtomicReference<>();
-        responseStatus.set(HttpStatus.NOT_FOUND);
+        HttpStatus responseStatus = reportService.deleteById(id)
+                .map( unused -> HttpStatus.OK )
+                .orElse( HttpStatus.NOT_FOUND );
 
-        reportRepository.findById(id).ifPresent(report -> {
-            report.getSchedules().stream().map(Schedule::getId).forEach(scheduleTaskService::removeTaskFromScheduler);
-            reportRepository.deleteById(id);
-            responseStatus.set(HttpStatus.OK);
-        });
-
-        return ResponseEntity.status(responseStatus.get()).build();
+        return ResponseEntity.status(responseStatus).build();
 
     }
 
